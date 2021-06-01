68ab3f4c7b77f927e52553f802dc498c7a5853f9
villivald <maxim.villivald@gmail.com>
2021-06-01 14:49:10 +0300

    :octocat:

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,23 @@
 module.exports = {
   meta: {
-    title: "My Purple Blog",
+    title: "Create React App Blog",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     lang: "en",
     siteUrl: "https://example.com/",
   },
-  feed: { // used in feed.xml.njk
+  feed: {
+    // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
     filename: "atom.xml",
     path: "/atom.xml",
     id: "https://example.com/",
     authorName: "John Doe",
-    authorEmail: "johndoe@example.com"
+    authorEmail: "johndoe@example.com",
   },
-  hero: { // used in hero section of main page ie. index.html.njk
-    title: "Welcome to my purple blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium deserunt odio esse."
-  }
-}
+  hero: {
+    // used in hero section of main page ie. index.html.njk
+    title: "Welcome to Create React App Blog",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium deserunt odio esse.",
+  },
+};
